88db2375d2f2ae18d2f55d1fbd142b69e1e192bb
s7aba <mohammedaziz.shaba@esprit.tn>
2021-04-27 13:56:49 +0100

    integration 2

---
+++ offre/Crud/src/controller/PannelAdminFXMLController.java
@@ -0,0 +1,196 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package controller;
+
+import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.control.Button;
+import javafx.scene.control.TitledPane;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.AnchorPane;
+
+/**
+ * FXML Controller class
+ *
+ * @author narje
+ */
+public class PannelAdminFXMLController implements Initializable {
+
+    @FXML
+    private TitledPane GestionPart;
+    @FXML
+    private Button ListeUsers;
+    @FXML
+    private Button ListePartner1;
+    @FXML
+    private TitledPane GestionEvent;
+    @FXML
+    private Button btnStand;
+    @FXML
+    private Button btnEvent;
+    @FXML
+    private Button FormStat;
+    @FXML
+    private TitledPane GestionOffre;
+    @FXML
+    private Button AjoutOff;
+    @FXML
+    private Button ConsulterOffres;
+    @FXML
+    private TitledPane GestionEntre;
+    @FXML
+    private Button ListEntr;
+    @FXML
+    private Button StatEntr;
+    @FXML
+    private TitledPane GestionReclam;
+    @FXML
+    private Button ListRecl;
+    @FXML
+    private Button StatRecl;
+    @FXML
+    private TitledPane GestionPubl;
+    @FXML
+    private Button ListPub;
+    @FXML
+    private Button StatPubl;
+    @FXML
+    private AnchorPane container_admin;
+    
+    public AnchorPane getContainer_admin() {
+        return container_admin;
+    }
+
+    public void setContainer_admin(AnchorPane container_admin) {
+        this.container_admin = container_admin;
+    }
+
+    /**
+     * Initializes the controller class.
+     */
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        GestionPart.setVisible(true);
+        
+        GestionEvent.setVisible(true);
+     
+         GestionOffre.setVisible(true);
+
+       GestionEntre.setVisible(true);
+    
+        GestionReclam.setVisible(true);
+       
+     GestionPubl.setVisible(true);
+    
+        
+        container_admin.setVisible(true);
+      
+        // TODO
+    }    
+
+    @FXML
+    private void accueilAdmin(MouseEvent event) {
+        try {
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("/gui/PannelAdmin.fxml"));       
+            Parent root = loader.load();
+            container_admin.getChildren().setAll(root);
+        } catch (IOException ex) {
+          
+        }
+    }
+ 
+    @FXML
+    private void Logout(ActionEvent event) {
+         try {
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("/gui/singnin.fxml"));
+            Parent root = loader.load();
+            //SingninController sc = loader.getController();
+
+              container_admin.getScene().setRoot(root);
+
+        } catch (IOException ex) {
+           
+        }
+    
+    }
+
+    @FXML
+    private void Listeusers(ActionEvent event) {
+    }
+
+    @FXML
+    private void ListePartner(ActionEvent event) {
+    }
+
+    @FXML
+    private void gest_form(ActionEvent event) {
+    }
+
+    @FXML
+    private void gest_event(ActionEvent event) {
+    }
+
+    @FXML
+    private void StatForm(ActionEvent event) {
+    }
+
+    @FXML
+    private void AjoutOffre(ActionEvent event) { 
+         try {
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("/gestion_offres/FXMLDocument.fxml"));       
+            Parent root = loader.load();
+            container_admin.getChildren().setAll(root);
+        } catch (IOException ex) {
+          
+        }
+        
+    }
+
+    @FXML
+    private void ConsulterOffres(ActionEvent event) {
+        try {
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("/gestion_offres/metiers.fxml"));       
+            Parent root = loader.load();
+            container_admin.getChildren().setAll(root);
+        } catch (IOException ex) {
+          
+        }
+        
+    }
+
+    @FXML
+    private void ListeEntre(ActionEvent event) {
+    }
+
+    @FXML
+    private void StatEntre(ActionEvent event) {
+    }
+
+    @FXML
+    private void ListRecl(ActionEvent event) {
+    }
+
+    @FXML
+    private void StatRecl(ActionEvent event) {
+    }
+
+    @FXML
+    private void ListPub(ActionEvent event) {
+    }
+
+    @FXML
+    private void StatPubli(ActionEvent event) {
+    }
+    
+}
---
+++ offre/Crud/src/controller/PannelEntrepriseController.java
@@ -0,0 +1,137 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package controller;
+
+import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.MenuItem;
+import javafx.scene.image.ImageView;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.HBox;
+
+/**
+ * FXML Controller class
+ *
+ * @author narje
+ */
+public class PannelEntrepriseController implements Initializable {
+       public static int idusers = 0;
+
+    @FXML
+    private HBox hboxmenu;
+    @FXML
+    private Button accueil;
+    @FXML
+    private MenuItem btEvent;
+    @FXML
+    private MenuItem btForm;
+    @FXML
+    private MenuItem btEvent1;
+    @FXML
+    private MenuItem btForm1;
+    @FXML
+    private Button forum;
+    @FXML
+    private HBox hboximage;
+    @FXML
+    private ImageView khedmtch;
+    @FXML
+    private ImageView login;
+    @FXML
+    private ImageView LogoutBox;
+    @FXML
+    private AnchorPane container_client;
+    @FXML
+    private Label iduser;
+
+    /**
+     * Initializes the controller class.
+     */
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+         //*idusers = SingninController.userIden;
+        login.setVisible(true);
+        LogoutBox.setVisible(true);
+    
+        //hboxout.setVisible(true);
+      
+           
+        // TODO
+    }    
+
+    @FXML
+    private void xxxxx(ActionEvent event) {
+        try {
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("/pidev/edu/khedmtech/gui/PanelEntrepriseFXML.fxml"));
+            Parent root = loader.load();
+              
+            container_client.getChildren().setAll(root);
+        } catch (IOException ex) {
+            Logger.getLogger(PannelClientController.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+
+    @FXML
+    private void ReclCandidat(ActionEvent event) {
+    }
+
+    @FXML
+    private void ReclFormation(ActionEvent event) {
+    }
+
+    @FXML
+    private void MesRecl(ActionEvent event) {
+    }
+
+    @FXML
+    private void StageOffre(ActionEvent event) {
+    }
+
+    @FXML
+    private void TravailOffre(ActionEvent event) {
+    }
+
+    @FXML
+    private void loadEvent(ActionEvent event) {
+    }
+
+    @FXML
+    private void loadFormation(ActionEvent event) {
+    }
+
+    @FXML
+    private void forum(ActionEvent event) {
+    }
+
+    @FXML
+    private void logDirection(MouseEvent event) {
+    }
+
+    @FXML
+    private void LogoutBox(MouseEvent event) {
+         try {
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("/pidev/edu/khedmtech/gui/singin.fxml"));
+            Parent root = loader.load();
+              
+           forum.getScene().setRoot(root);
+        } catch (IOException ex) {
+            Logger.getLogger(PannelClientController.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+    }
+    
+
